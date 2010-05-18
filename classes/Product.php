6ba782d815a3f4dcdc07ed49141e12474211d988
Benson <bensonby@bensonby-desktop.(none)>
2010-05-18 19:37:37 +0800

    add the new class Product, change it to OOP, and modify the product page accordingly

--- classes/Product.php
+++ classes/Product.php
@@ -1,11 +1,37 @@
 <?php
 class Product {
+
+  static function find($id){
+    $id = mysql_real_escape_string($id);
+    if(empty($id)) return false;
+    $result = sql("SELECT * FROM products WHERE id = $id", SQL_SINGLE_ROW);
+    if(!$result){
+      log2("Invalid SQL query -- ".mysql_error().": id = $id");
+      return false;
+    }
+    $ret = new Product($result);
+    return $ret;
+  }
+
+  private function __construct($info){
+    $this->name = $info["name"];
+    $this->description = $info["description"];
+    $this->price = $info["price"];
+    $this->sample_image = $info["sample_image"];
+    $this->attr_list = explode(",", $info["attr_list"]);
+  }
+
   static function get_all_products(){
     $products = sql("SELECT * FROM products ORDER BY id");
-    for($i=0; $i<count($products); $i++){
-      $products[$i]["attr_list"] = explode(",", $products[$i]["attr_list"]);
-    }
-    return $products;
+    foreach($products as $product) $ret[] = new Product($product);
+    return $ret;
   }
+
+  function get_id(){ return $this->id; }
+  function get_name(){ return $this->name; }
+  function get_description(){ return $this->description; }
+  function get_price(){ return $this->price; }
+  function get_sample_image(){ return $this->sample_image; }
+  function get_attr_list(){ return $this->attr_list; }
 }
 ?>
--- products.php
+++ products.php
@@ -13,16 +13,22 @@ include 'menu.php';
   <? foreach($products as $product){ ?>
     <div class="products-block">
       <div class="products-img-container"><!-- perhaps 128x128 pixels? -->
-        <img src="<?=$product["sample_image"] ?>" />
+        <img src="<?php echo $product->get_sample_image(); ?>" />
       </div>
       <div class="products-desc-container">
-        <h4><?=$product["name"] ?></h4>
+        <h4><?php echo $product->get_name(); ?></h4>
         <h5>Description: </h5>
-          <span class="products-desc-text"><?=$product["description"] ?></span>
+          <span class="products-desc-text"><?php echo $product->get_description(); ?></span>
         <h5>Customizable Features: </h5>
-           <span class="products-attr-list"><?=implode(", ", $product["attr_list"]) ?></span>
+          <span class="products-attr-list"><?php echo implode(", ", $product->get_attr_list()); ?></span>
         <h5>Price: </h5>
-           <span class="products-price-text"><?=$product["price"] ?></span>
+           <span class="products-price-text"><?php echo $product->get_price(); ?></span>
+      </div>
+      <div class="products-buy-container">
+        <a onclick="show_product_box(<?php echo $product->get_id(); ?>); return false" 
+           href="add_product.php?id=<?php echo $product->get_id(); ?>">
+          <img src="images/cart.gif" alt="cart icon" /> Add to Cart
+        </a>
       </div>
     </div>
   <? } ?>

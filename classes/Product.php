1fec659f22ce8c0d42361457e8c09f03451a4f74
Benson <bensonby@bensonby-desktop.(none)>
2010-05-18 16:21:48 +0800

    make the layout (html only) of the page Products

--- classes/Product.php
+++ classes/Product.php
@@ -2,7 +2,9 @@
 class Product {
   static function get_all_products(){
     $products = sql("SELECT * FROM products ORDER BY id");
-    $products["attr_list"] = explode(",", $products["attr_list"]);
+    for($i=0; $i<count($products); $i++){
+      $products[$i]["attr_list"] = explode(",", $products[$i]["attr_list"]);
+    }
     return $products;
   }
 }

7accdc4f082ffac20cfd661110341e64d2b41347
Benson <bensonby@bensonby-desktop.(none)>
2010-05-18 21:52:59 +0800

    Fix a big in the class Product, of missing to initialize ID on __construct

--- classes/Product.php
+++ classes/Product.php
@@ -14,6 +14,7 @@ class Product {
   }
 
   private function __construct($info){
+    $this->id = $info["id"];
     $this->name = $info["name"];
     $this->description = $info["description"];
     $this->price = $info["price"];

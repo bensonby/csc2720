d6390394582d36f066abbaa250bf9723698c91cc
Benson <bensonby@bensonby-desktop.(none)>
2010-05-19 17:12:17 +0800

    Fix bugs in classes Cap and Shirt (due to conflicts)

--- classes/Cap.php
+++ classes/Cap.php
@@ -1,7 +1,6 @@
 <?php
 
 class Cap extends CusProduct {
-<<<<<<< HEAD:classes/Cap.php
   protected $attr=array("size"=>array('Small', 'Medium', 'Large'),
                         "color"=>array('Black', 'Blue', 'Brown', 'Red', 'White'),
                         "text"=>15,
@@ -9,15 +8,4 @@ class Cap extends CusProduct {
                        );
 
 }
-=======
-  //private $product_id=3;
-  //private $product_name="cap";
-  protected $attr=array("size"=>array('Small', 'Medium', 'Large'),
-                      "type"=>array('type 1', 'type 2', 'type 3'),
-                      "image"=>array('hello.jpg')
-                     );
-
-
-}
 ?>
->>>>>>> 00420673697a197712f11a23c3390aa49fc162e2:classes/Cap.php

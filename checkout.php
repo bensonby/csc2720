1ed7c9c7dc1ec08c6cf88eb629ac56aa4dfe9a3c
Benson <bensonby@bensonby-desktop.(none)>
2010-05-20 22:57:33 +0800

    checkout

--- checkout.php
+++ checkout.php
@@ -1,6 +1,32 @@
 <?php
 include 'inc.php';
 
+$order_id=Order::get_orderid($_SESSION['user_id']);
+if (!empty($order_id)){
+  $order=Order::find($order_id);
+  $cus_products=$order->get_cus_product();
+}
+
+if (empty($cus_products)){
+  header("Location: cart.php");
+  exit();
+}
+$keys=array("name","phone","email");
+$error=array_fill_keys($keys,false);
+if (Validation::name($_POST["name"]))
+  $order->set_name($_POST["name"]);
+else $error["name"]=true;
+$order->set_address($_POST["address"]);
+if (Validation::phone($_POST["phone"]))
+  $order->set_phone($_POST["phone"]);
+else $error["phone"]=true;
+if (Validation::email($_POST["email"])) 
+  $order->set_email($_POST["email"]);
+else $error["email"]=true;
+Validation::testing("order",$order);
+
+
+
 include 'header.php';
 
 include 'menu.php';
@@ -8,7 +34,7 @@ include 'menu.php';
 
 <div id="content">
  <h3>Checkout Form</h3> 
-  <form>
+  <form action="checkout.php" method="POST">
      <table>
        <tr>
           <td class="row1">Name:</td>

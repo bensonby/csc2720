63cf12e0e52d5caf8ec50e94774df4fdf08fb207
Benson <bensonby@bensonby-desktop.(none)>
2010-05-20 23:16:40 +0800

    checkout

--- checkout.php
+++ checkout.php
@@ -12,19 +12,33 @@ if (empty($cus_products)){
   exit();
 }
 $keys=array("name","phone","email");
-$error=array_fill_keys($keys,false);
+$error=false;
 if (Validation::name($_POST["name"]))
   $order->set_name($_POST["name"]);
-else $error["name"]=true;
+else{
+  $error=true;
+  $msg="Name format incorrect<br>";
+}
 $order->set_address($_POST["address"]);
 if (Validation::phone($_POST["phone"]))
   $order->set_phone($_POST["phone"]);
-else $error["phone"]=true;
+else{
+  $error=true;
+  $msg=$msg."Phone format incorrect<br>";
+}
 if (Validation::email($_POST["email"])) 
   $order->set_email($_POST["email"]);
-else $error["email"]=true;
-Validation::testing("order",$order);
+else{
+  $error=true;
+  $msg=$msg."Email format incorrect<br>";
+}
 
+if ($error)
+  set_msg("<h4>$msg</h4>");
+else{
+  set_msg("<h4>Order completed</h4>");
+
+}
 
 
 include 'header.php';
@@ -38,19 +52,19 @@ include 'menu.php';
      <table>
        <tr>
           <td class="row1">Name:</td>
-          <td class="row2"><input type="text" name="name"/> </td>
+          <td class="row2"><input type="text" name="name" value="<?php echo $_POST["name"]; ?>" /> </td>
        </tr>
        <tr>
           <td class="row1">Receiver's Address:</td>
-          <td class="row2"><input type="text" name="address"/> </td>
+          <td class="row2"><input type="text" name="address" value="<?php echo $_POST["address"]; ?>" /> </td>
        </tr>
        <tr>
           <td class="row1">Phone:</td>
-          <td class="row2"><input type="text" name="phone"/> </td>
+          <td class="row2"><input type="text" name="phone" value="<?php echo $_POST["phone"]; ?>" /> </td>
        </tr>
        <tr>
           <td class="row1">email:</td>
-          <td class="row2"><input type="text" name="email"/> </td>
+          <td class="row2"><input type="text" name="email" value="<?php echo $_POST["email"]; ?>"/> </td>
        </tr>
        <tr>
           <td colspan="2"><input type="submit" value="Submit" /><input type="reset" value="Reset" /><input type="button" value="Back" /></td>

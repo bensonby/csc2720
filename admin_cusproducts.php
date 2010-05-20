846fb0e135930b7482d004f1e0456b7b052088f4
Benson <bensonby@bensonby-desktop.(none)>
2010-05-20 18:45:22 +0800

    progress on admin CusProduct search

--- admin_cusproducts.php
+++ admin_cusproducts.php
@@ -19,51 +19,52 @@ include 'menu.php';
 ?>
 <div id="content">
   <div id="admin-menu">
-  <h2>Admin Panel</h2>
-      <div class="admin-menu-item"><a href="admin_orders.php">List Orders</a></div>
-<!--      <div class="admin-menu-item"><a href="admin_orders_search.php">Search Orders</a></div>-->
-      <div class="admin-menu-item"><a href="admin_cusproducts.php">List Customized Products</a></div>
-      <div class="admin-menu-item"><a href="admin_cusproducts_search.php">Search Customized Products</a></div>
-    </div>
-    <div id="admin-content">
-      <form id="admin-cusproducts-search-form" action="admin_cusproducts.php" method="post">
-        <h2>Search Committed Customized Products</h2>
-        Leave the field empty for no restrictions<br />
-        <table border="0">
-          <tr><td><label for="product_ids[]">Product Types: </label>
-                  <select name="products_ids[]" multiple="multiple">
-                    <?php foreach($products as $p){ ?>
-                      <option value="<?php echo $p->get_id(); ?>"><?php echo $p->get_name(); ?></option>
-                    <?php } ?>
-              </td>
-          </tr>
-       </table>
-       <input type="submit" name="submit" value="Search Orders" />
-      </form>
-    </div>
-  </h2>
+    <h2>Admin Panel</h2>
+    <div class="admin-menu-item"><a href="admin_orders.php">List/Search Orders</a></div>
+    <div class="admin-menu-item"><a href="admin_cusproducts.php">List/Search Customized Products</a></div>
+  </div>
+  <div id="admin-content">
+    <form id="admin-cusproducts-search-form" action="admin_cusproducts.php" method="post">
+      <h2>Search Committed Customized Products</h2>
+      Leave the field empty for no restrictions<br />
+      <table border="0">
+        <tr><td><label for="product_ids[]">Product Types: </label>
+                <select name="products_ids[]" multiple="multiple">
+                  <?php foreach($products as $p){ ?>
+                    <option value="<?php echo $p->get_id(); ?>"><?php echo $p->get_name(); ?></option>
+                  <?php } ?>
+            </td>
+        </tr>
+     </table>
+     <input type="submit" name="submit" value="Search Orders" />
+    </form>
 
-  <h2>Customized Products</h2>
-  <?php if(empty($cus_products)){ ?>
-    No results found. Please try another search.
-  <?php }else{ ?>
-    <table border="0">
-      <tr><td>Details</td><td>ID</td><td>User</td><td>Product</td><td>Quantity</td><td>Attributes</td></tr>
-  <?php  foreach($cus_products as $p){ ?>
-      <tr>
-        <td><a href="admin_cusproduct.php?id=<?php echo $p->get_id(); ?>">details</a></td>
-        <td><?php echo $p->get_id(); ?></td>
-        <td><?php echo $p->find_username(); ?></td>
-        <td><?php echo $p->get_name(); ?></td>
-        <td><?php echo $p->get_quantity(); ?></td>
-        
-        <td><?php foreach($p->get_custom() as $key=>$value){ ?>
-              <?php echo $key; ?>: <em><?php echo $value; ?></em><br />
-            <?php } ?>
-        </td>
-      </tr>
-  <?php } ?>
-  <?php } ?>
+    <h2>Customized Products</h2>
+    <?php if(empty($cus_products)){ ?>
+      No results found. Please try another search.
+    <?php }else{ ?>
+      <table border="0">
+        <tr><td>Details</td><td>ID</td><td>User</td><td>Product</td><td>Quantity</td><td>Attributes</td></tr>
+    <?php  foreach($cus_products as $p){ ?>
+        <tr>
+          <td rowspan="2"><a href="admin_cusproduct.php?id=<?php echo $p->get_id(); ?>">details</a></td>
+          <td rowspan="2"><?php echo $p->get_id(); ?></td>
+          <td rowspan="2"><?php echo $p->find_username(); ?></td>
+          <td rowspan="2"><?php echo $p->get_name(); ?></td>
+          <td rowspan="2"><?php echo $p->get_quantity(); ?></td>
+          
+          <?php foreach($p->get_custom() as $key=>$value){ ?>
+          <td><?php echo $key; ?></td>
+          <?php } ?>
+        </tr>
+        <tr>
+          <?php foreach($p->get_custom() as $key=>$value){ ?>
+          <td><em><?php echo $value; ?></em></td>
+          <?php } ?>
+        </tr>
+    <?php } ?>
+    <?php } ?>
+  </div>
     
 </div>
 
